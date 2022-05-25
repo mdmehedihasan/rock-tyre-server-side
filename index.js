02ecdba8656aab38e3f311937a7056da8ad1df63
mehedi hasan <mehedicsebu@gmail.com>
2022-05-25 12:18:41 +0600

    get product with id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -40,6 +40,14 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
         });
+
+        //get with id
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
 
 
         //Post review
