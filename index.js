e9778f9775c9b6f8f65d8ad37e0fc0f5996563df
mehedi hasan <mehedicsebu@gmail.com>
2022-05-27 10:37:55 +0600

    only admin can get access all the user route

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
             const orders = await orderCollection.find(query).toArray();
             res.send(orders);
         });
+        //only admin can get access the all user route
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
         //put for new user
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
